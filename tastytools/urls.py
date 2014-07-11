4d9c39ca15acfaa0ba6c87f6ba317e455f2b6c3a
Tuan Tran <tuan@velodee.com>
2014-07-11 17:12:19 +0700

    Update URL

--- tastytools/urls.py
+++ tastytools/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
-from views import doc, howto
+from .views import doc
 
 urlpatterns = patterns('',
-    (r'^doc', doc),
-    (r'^howto', howto),
+    (r'^', doc),
 )
